974e00d66cd966ad4fb5257e5fa035918b190282
sebasmazo <sebastianmazovelez@gmail.com>
2022-04-28 23:42:11 -0500

    v 0.3
    Navbar done

--- search.php
+++ search.php
@@ -1,22 +1,21 @@
 <?php
+    require "dbConn.php";
     require "functions.php";
 ?>
 <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Walletz</title>
-        
-        <link rel="icon" type="image/x-icon" href="./imgs/LogoWalletz.ico">
-        <link rel="stylesheet" href="./css/main.css">
-    </head>
-    <header>
-        <div class="navbar">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<header>
+    <div class="navbar">
             <div class="container-nav">
-                <div class="menu" id="active"><a href="index.php">Inicio</a></div>
-                <div class="menu"><a href="search.php">Buscar reseña</a></div>
+                <div class="menu" ><a href="index.php">Inicio</a></div>
+                <div class="menu" id="active"><a href="search.php">Buscar reseña</a></div>
                 <div class="menu"><a href="#">Crear reseña</a></div>
                 <div class="menu"><a href="#">Perfil</a></div>
                 <div class="menu" id="right-item">
@@ -30,8 +29,8 @@
                 </div>
             </div>
         </div>
-    </header>
-    <body>
-        <h1></h1>
-    </body>
+</header>
+<body>
+    
+</body>
 </html>
