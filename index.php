21f242bbc35e3bfd7f1055c8386b3c2c0165c5a9
sebasmazo <sebastianmazovelez@gmail.com>
2022-04-29 22:39:03 -0500

    v 0.4
    Integration with Invest.com iframe

--- index.php
+++ index.php
@@ -10,14 +10,13 @@
         <title>Walletz</title>
         
         <link rel="icon" type="image/x-icon" href="./imgs/LogoWalletz.ico">
-        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="css/main.css">
     </head>
     <header>
         <div class="navbar">
             <div class="container-nav">
                 <div class="menu" id="active"><a href="index.php">Inicio</a></div>
-                <div class="menu"><a href="search.php">Buscar reseña</a></div>
-                <div class="menu"><a href="#">Crear reseña</a></div>
+                <div class="menu"><a href="search.php">Reseñas</a></div>
                 <div class="menu"><a href="#">Perfil</a></div>
                 <div class="menu" id="right-item">
                     <?php 
@@ -32,6 +31,16 @@
         </div>
     </header>
     <body>
-        <h1></h1>
+        <div class="container-info">
+                        <div class="flex-item" id="item1"><h1>Walletz</h1><img id="logo1" src="imgs/LogoWalletz-sinfondo.png"></div>
+                        <div class="flex-item" id="item2"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla inventore vitae laborum reprehenderit, facilis deleniti dolorum magni voluptatum corporis corrupti, eos placeat? Modi perferendis magni adipisci itaque numquam. Labore, alias.</p></div>
+        </div>
+        <div class="external-source">
+            <iframe src="https://es.widgets.investing.com/top-cryptocurrencies?theme=darkTheme&cols=symbol,priceUsd,vol24,chg24" width="100%" height="480px" frameborder="0" allowtransparency="true" marginwidth="0" marginheight="0"></iframe><div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;">Cortesía de <a href="https://es.investing.com?utm_source=WMT&amp;utm_medium=referral&amp;utm_campaign=TOP_CRYPTOCURRENCIES&amp;utm_content=Footer%20Link" target="_blank" rel="nofollow">Investing.com</a></div>
+        </div>            
+        
     </body>
+    <footer>
+        <div class="copyright">@2022 Sebastian Mazo - Juan Sebastian Serna Correa</div>
+    </footer>
 </html>
