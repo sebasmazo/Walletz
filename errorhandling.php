efb5d3504ac200aff9489c568e38230f1fe5dc1e
sebasmazo <sebastianmazovelez@gmail.com>
2022-05-21 12:03:29 -0500

    Update errorhandling.php

--- errorhandling.php
+++ errorhandling.php
@@ -31,7 +31,7 @@ echo
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Walletz</title>
     <link rel="icon" type="image/x-icon" href="./imgs/LogoWalletz.ico">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/main.css"> 
 </head>
 <body>
     
